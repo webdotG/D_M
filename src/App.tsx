a1d436799fe7d27a31e5bde68c5f348bb0527287
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-14 14:32:01 +0300

    test work route gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/welcome/welcomePage';
 import LoginRegisterPage from './pages/login_register/loginRegisterPage';
 import HomePage from './pages/home/home';
 import Layout from './components/layout/layout';
 
 function App() {
-
   return (
-    <Routes>
-      <Route index path="/D_M/welcome" element={<WelcomePage />} />
-      <Route index path="/D_M/login" element={<LoginRegisterPage />} />
-      <Route index path='/D_M/' element={<Layout><HomePage /></Layout>} />
-    </Routes>
-  )
+    <Router basename="/D_M">
+      <Routes>
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginRegisterPage />} />
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './variables.module.scss'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+
+
+// import { BrowserRouter } from 'react-router-dom'
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// )
+
+
+import React from 'react';
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
