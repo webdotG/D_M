ad40c41fd8b53f4faea5f76eac70785b4ea00765
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-14 20:29:23 +0300

    create store zustand test category theme lang

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,47 +1,71 @@
-import { ReactNode, useState, useEffect } from 'react'
-import style from './layout.module.scss'
-import Header from '../components/header/header'
-import Footer from '../components/footer/footer'
-import useCategoryStore from '../store.ts';
-
+import { ReactNode, useState, useEffect } from 'react';
+import style from './layout.module.scss';
+import Header from '../components/header/header';
+import Footer from '../components/footer/footer';
+import { useCategoryStore, useLanguageStore, useThemeStore } from '../store.ts';
 
 type typeProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-const CATEGORY = ['cны', 'воспоминания']
+const CATEGORY = ['сны', 'воспоминания'];
+const LANGUAGES = ['Русский', 'English', 'Morder'];
+const THEMES = ['Светлая', 'Тёмная', 'Мордер'];
 
 export default function Layout({ children }: typeProps) {
   const { selectedCategory, setSelectedCategory } = useCategoryStore();
-  const [modalVisible, setModalVisible] = useState(false);
+  const { language, setLanguage } = useLanguageStore();
+  const { theme, setTheme } = useThemeStore();
 
-  const toggleModal = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [languageModalVisible, setLanguageModalVisible] = useState(false);
+  const [themeModalVisible, setThemeModalVisible] = useState(false);
+
+  const toggleCategoryModal = () => {
     setModalVisible(!modalVisible);
   };
+
+  const toggleLanguageModal = () => {
+    setLanguageModalVisible(!languageModalVisible);
+  };
+
+  const toggleThemeModal = () => {
+    setThemeModalVisible(!themeModalVisible);
+  };
+
   const handleCategorySelect = (category: string) => {
     setSelectedCategory(category);
     setModalVisible(false);
   };
 
+  const handleLanguageSelect = (language: string) => {
+    setLanguage(language);
+    setLanguageModalVisible(false);
+  };
+
+  const handleThemeSelect = (theme: "Светлая" | "Тёмная" | "Мордер") => {
+    setTheme(theme); 
+    setThemeModalVisible(false);
+  };
+  
+
   useEffect(() => {
-    console.log('layout useEffect selectedCategory : ', selectedCategory);
-    const currentState = useCategoryStore.getState();
-    console.log('layout useEffect currentState : ', currentState);
-  }, [selectedCategory]);
+    console.log('layout useEffect selectedCategory, language , theme : ', selectedCategory, language, theme);
+  }, [selectedCategory, language, theme]);
 
   return (
-
     <div className={style['layout']}>
       <div className={style['layout__content']}>
 
+        {/* Категория */}
         <section className={style['layout__categories']}>
-
           {modalVisible && (
-            <div className={style.modal_overlay} onClick={toggleModal}>
+            <div className={style.modal_overlay} onClick={toggleCategoryModal}>
               <ul className={style.layout__modal}>
                 {CATEGORY.map((category, index) => (
                   <li key={index} className={style.layout__modal__item}>
-                    <button className={style.modalToggle_btn}
+                    <button
+                      className={style.modalToggle_btn}
                       onClick={() => handleCategorySelect(category)}
                     >
                       {category}
@@ -52,56 +76,64 @@ export default function Layout({ children }: typeProps) {
             </div>
           )}
 
-
-          <button className={style['categoryToggle-btn']} onClick={toggleModal}>
+          <button className={style['categoryToggle-btn']} onClick={toggleCategoryModal}>
             <svg viewBox="0 -6 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-              <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <g transform="translate(-204.000000, -365.000000)"
-                  fill="#000000">
-                  <path d="M214,379 C211.791,379 210,377.209 210,375 C210,372.791 211.791,371 214,371 C216.209,371 218,372.791 218,375 C218,377.209 216.209,379 214,379 L214,379 Z M214,369 C210.687,369 208,371.687 208,375 C208,378.313 210.687,381 214,381 C217.314,381 220,378.313 220,375 C220,371.687 217.314,369 214,369 L214,369 Z M226,383 L214,383 C209.582,383 206,379.418 206,375 C206,370.582 209.582,367 214,367 L226,367 C230.418,367 234,370.582 234,375 C234,379.418 230.418,383 226,383 L226,383 Z M226,365 L214,365 C208.477,365 204,369.478 204,375 C204,380.522 208.477,385 214,385 L226,385 C231.523,385 236,380.522 236,375 C236,369.478 231.523,365 226,365 L226,365 Z">
-                  </path>
-                </g>
-              </g>
+              <text x="0" y="20" fill="#000000">{selectedCategory}</text>
             </svg>
           </button>
-
-          <p className={style['layout__categories-target']}>
-            {selectedCategory}
-          </p>
-
         </section>
 
+        {/* Цветовая тема */}
+        <section className={style['layout__color-theme']}>
+          {themeModalVisible && (
+            <div className={style.modal_overlay} onClick={toggleThemeModal}>
+              <ul className={style.layout__modal}>
+                {THEMES.map((theme, index) => (
+                  <li key={index} className={style.layout__modal__item}>
+                    <button className={style.modalToggle_btn} onClick={() => handleThemeSelect(theme)}>
+                      {theme}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
 
+          <button className={style['colorThemeToggle-btn']} onClick={toggleThemeModal}>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <text x="0" y="20" fill="#000000">{theme}</text>
+            </svg>
+          </button>
+        </section>
 
-        <ul className={style['layout__toggle']}>
-          <li className={style['layout__toggle__item']}>
-            <button className={style['langToggle-btn']}>
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M20.58 19.37L17.59 11.01C17.38 10.46 16.91 10.12 16.37 10.12C15.83 10.12 15.37 10.46 15.14 11.03L12.16 19.37C12.02 19.76 12.22 20.19 12.61 20.33C13 20.47 13.43 20.27 13.57 19.88L14.19 18.15H18.54L19.16 19.88C19.27 20.19 19.56 20.38 19.87 20.38C19.95 20.38 20.04 20.37 20.12 20.34C20.51 20.2 20.71 19.77 20.57 19.38L20.58 19.37ZM14.74 16.64L16.38 12.05L18.02 16.64H14.74ZM12.19 7.85C9.92999 11.42 7.89 13.58 5.41 15.02C5.29 15.09 5.16 15.12 5.04 15.12C4.78 15.12 4.53 14.99 4.39 14.75C4.18 14.39 4.3 13.93 4.66 13.73C6.75999 12.51 8.48 10.76 10.41 7.86H4.12C3.71 7.86 3.37 7.52 3.37 7.11C3.37 6.7 3.71 6.36 4.12 6.36H7.87V4.38C7.87 3.97 8.21 3.63 8.62 3.63C9.02999 3.63 9.37 3.97 9.37 4.38V6.36H13.12C13.53 6.36 13.87 6.7 13.87 7.11C13.87 7.52 13.53 7.86 13.12 7.86H12.18L12.19 7.85ZM12.23 15.12C12.1 15.12 11.97 15.09 11.85 15.02C11.2 14.64 10.57 14.22 9.97999 13.78C9.64999 13.53 9.58 13.06 9.83 12.73C10.08 12.4 10.55 12.33 10.88 12.58C11.42 12.99 12.01 13.37 12.61 13.72C12.97 13.93 13.09 14.39 12.88 14.75C12.74 14.99 12.49 15.12 12.23 15.12Z"
-                  fill="#000000"
-                />
-              </svg>
-            </button>
-          </li>
-          <li className={style['layout__toggle__item']}>
-            <button className={style['colorToggle-btn']}>
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="12" cy="12" r="9" stroke="#33363F" strokeWidth="2" />
-                <path d="M18.364 5.63604C16.6761 3.94821 14.3869 3 12 3C9.61305 3 7.32387 3.94821 5.63604 5.63604C3.94821 7.32387 3 9.61305 3 12C3 14.3869 3.94821 16.6761 5.63604 18.364L12 12L18.364 5.63604Z"
-                  fill="#33363F"
-                />
-              </svg>
-            </button>
-          </li>
-        </ul>
+        {/* Язык */}
+        <section className={style['layout__language']}>
+          {languageModalVisible && (
+            <div className={style.modal_overlay} onClick={toggleLanguageModal}>
+              <ul className={style.layout__modal}>
+                {LANGUAGES.map((lang, index) => (
+                  <li key={index} className={style.layout__modal__item}>
+                    <button className={style.modalToggle_btn} onClick={() => handleLanguageSelect(lang)}>
+                      {lang}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          <button className={style['languageToggle-btn']} onClick={toggleLanguageModal}>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <text x="0" y="20" fill="#000000">{language}</text>
+            </svg>
+          </button>
+        </section>
 
       </div>
 
       <Header />
       {children}
       <Footer />
-
     </div>
-  )
-
+  );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,19 +1,44 @@
-import { create } from 'zustand';
-
-export const CATEGORY: string[] = ['сны', 'воспоминания'];
+import { create }  from 'zustand';
 
 type CategoryState = {
   selectedCategory: string;
   setSelectedCategory: (category: string) => void;
 };
 
-const useCategoryStore = create<CategoryState>((set, get) => ({
+type LanguageState = {
+  language: string;
+  setLanguage: (language: string) => void;
+};
+
+type ThemeState = {
+  theme: "Светлая" | "Тёмная" | "Мордер"; 
+  setTheme: (theme: "Светлая" | "Тёмная" | "Мордер") => void;
+};
+
+const CATEGORY = ['сны', 'воспоминания'];
+const LANGUAGES = ['Русский', 'English', 'Morder'];
+const THEMES = ['Светлая', 'Тёмная', 'Мордер'];
+
+export const useCategoryStore = create<CategoryState>((set) => ({
   selectedCategory: CATEGORY[0],
   setSelectedCategory: (category: string) => {
+    console.log(`Store selectedCategory : ${category}`);
     set({ selectedCategory: category });
-    const currentState = get();
-    console.log('STORE currentState : ', currentState);
   },
 }));
 
-export default useCategoryStore;
+export const useLanguageStore = create<LanguageState>((set) => ({
+  language: LANGUAGES[0],
+  setLanguage: (language: string) => {
+    console.log(`Store language : ${language}`);
+    set({ language });
+  },
+}));
+
+export const useThemeStore = create<ThemeState>((set) => ({
+  theme: THEMES[1],
+  setTheme: (theme: 'Светлая' | 'Тёмная' | 'Мордер') => {
+    console.log(`Store theme :  ${theme} `);
+    set({ theme });
+  },
+}));
