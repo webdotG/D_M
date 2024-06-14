b8515f62aede45e3a615d492b2794f868be6784c
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-14 17:38:01 +0300

    layout html add

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -4,24 +4,76 @@ import Header from '../header/header'
 import Footer from '../footer/footer'
 import { Link } from 'react-router-dom'
 
+
 type typeProps = {
   children: ReactNode
 }
 
 export default function Layout({ children }: typeProps) {
 
+  const CATEGORY = ['cны', 'воспоминания']
+
   return (
 
     <div className={style['layout']}>
+      <div className={style['layout__content']}>
 
-      <ul className={style['layout__content']}>
-        <li className={style['layout__content__item']}>
-          <Link to='/'>cны </Link>
-        </li>
-        <li className={style['layout__content__item']}>
-          <Link to='/'>воспоминания </Link>
-        </li>
-      </ul>
+        <section className={style['layout__categories']}>
+
+          <ul className={style['layout__modal']}>
+            {CATEGORY.map((category, index) => (
+              <li key={index} className={style.layout__modal__item}>
+                <button className={style['modalToggle_btn']}>
+                  {category}
+                </button>
+              </li>
+            ))}  
+          </ul>
+
+          <button className={style['categoryToggle-btn']} aria-label="Переключить категории">
+            <svg viewBox="0 -6 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <g transform="translate(-204.000000, -365.000000)"
+                  fill="#000000">
+                  <path d="M214,379 C211.791,379 210,377.209 210,375 C210,372.791 211.791,371 214,371 C216.209,371 218,372.791 218,375 C218,377.209 216.209,379 214,379 L214,379 Z M214,369 C210.687,369 208,371.687 208,375 C208,378.313 210.687,381 214,381 C217.314,381 220,378.313 220,375 C220,371.687 217.314,369 214,369 L214,369 Z M226,383 L214,383 C209.582,383 206,379.418 206,375 C206,370.582 209.582,367 214,367 L226,367 C230.418,367 234,370.582 234,375 C234,379.418 230.418,383 226,383 L226,383 Z M226,365 L214,365 C208.477,365 204,369.478 204,375 C204,380.522 208.477,385 214,385 L226,385 C231.523,385 236,380.522 236,375 C236,369.478 231.523,365 226,365 L226,365 Z">
+                  </path>
+                </g>
+              </g>
+            </svg>
+          </button>
+
+          <p className={style['layout__categories-target']}>
+            {CATEGORY[0]}
+          </p>
+
+        </section>
+
+
+
+        <ul className={style['layout__toggle']}>
+          <li className={style['layout__toggle__item']}>
+            <button className={style['langToggle-btn']}>
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M20.58 19.37L17.59 11.01C17.38 10.46 16.91 10.12 16.37 10.12C15.83 10.12 15.37 10.46 15.14 11.03L12.16 19.37C12.02 19.76 12.22 20.19 12.61 20.33C13 20.47 13.43 20.27 13.57 19.88L14.19 18.15H18.54L19.16 19.88C19.27 20.19 19.56 20.38 19.87 20.38C19.95 20.38 20.04 20.37 20.12 20.34C20.51 20.2 20.71 19.77 20.57 19.38L20.58 19.37ZM14.74 16.64L16.38 12.05L18.02 16.64H14.74ZM12.19 7.85C9.92999 11.42 7.89 13.58 5.41 15.02C5.29 15.09 5.16 15.12 5.04 15.12C4.78 15.12 4.53 14.99 4.39 14.75C4.18 14.39 4.3 13.93 4.66 13.73C6.75999 12.51 8.48 10.76 10.41 7.86H4.12C3.71 7.86 3.37 7.52 3.37 7.11C3.37 6.7 3.71 6.36 4.12 6.36H7.87V4.38C7.87 3.97 8.21 3.63 8.62 3.63C9.02999 3.63 9.37 3.97 9.37 4.38V6.36H13.12C13.53 6.36 13.87 6.7 13.87 7.11C13.87 7.52 13.53 7.86 13.12 7.86H12.18L12.19 7.85ZM12.23 15.12C12.1 15.12 11.97 15.09 11.85 15.02C11.2 14.64 10.57 14.22 9.97999 13.78C9.64999 13.53 9.58 13.06 9.83 12.73C10.08 12.4 10.55 12.33 10.88 12.58C11.42 12.99 12.01 13.37 12.61 13.72C12.97 13.93 13.09 14.39 12.88 14.75C12.74 14.99 12.49 15.12 12.23 15.12Z"
+                  fill="#000000"
+                />
+              </svg>
+            </button>
+          </li>
+          <li className={style['layout__toggle__item']}>
+            <button className={style['colorToggle-btn']}>
+              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="12" cy="12" r="9" stroke="#33363F" strokeWidth="2" />
+                <path d="M18.364 5.63604C16.6761 3.94821 14.3869 3 12 3C9.61305 3 7.32387 3.94821 5.63604 5.63604C3.94821 7.32387 3 9.61305 3 12C3 14.3869 3.94821 16.6761 5.63604 18.364L12 12L18.364 5.63604Z"
+                  fill="#33363F"
+                />
+              </svg>
+            </button>
+          </li>
+        </ul>
+
+      </div>
+      
       <Header />
       {children}
       <Footer />
