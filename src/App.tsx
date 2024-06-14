0bda77df4859defe0fa72d37849af15e0a7076b9
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-14 14:44:44 +0300

    dev mode route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './pages/welcome/welcomePage';
 import LoginRegisterPage from './pages/login_register/loginRegisterPage';
 import HomePage from './pages/home/home';
@@ -6,13 +6,11 @@ import Layout from './components/layout/layout';
 
 function App() {
   return (
-    <Router basename="/D_M">
       <Routes>
-        <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/login" element={<LoginRegisterPage />} />
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
+        <Route path="/D_M/welcome" element={<WelcomePage />} />
+        <Route path="/D_M/login" element={<LoginRegisterPage />} />
+        <Route path="/D_M/" element={<Layout><HomePage /></Layout>} />
       </Routes>
-    </Router>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,26 +3,11 @@ import App from './App.tsx'
 import './index.css'
 import './variables.module.scss'
 
+import { BrowserRouter } from 'react-router-dom'
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
-// import { BrowserRouter } from 'react-router-dom'
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-// )
-
-
-import React from 'react';
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error('Root element not found');
-}
