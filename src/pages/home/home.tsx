5e9a674f2bf0a13f4406879cfc0e63de365911bb
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-18 21:43:22 +0300

    add black speech translit

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -9,9 +9,18 @@ export default function HomePage() {
   const { language } = useLanguageStore();
 
   
-  const translateToLanguage = (text: string) => {
-    const languageTranslations = translations[language] || translations.RU;
-    return languageTranslations[text] || text;
+  const translateToLanguage = (text: string): string => {
+    if (!(language in translations)) {
+      throw new Error(`Язык '${language}' не поддерживается.`);
+    }
+  
+    const languageTranslations = translations[language as keyof typeof translations];
+  
+    if (!(text in languageTranslations)) {
+      throw new Error(`Перевод для текста '${text}' не найден в языке '${language}'.`);
+    }
+  
+    return languageTranslations[text as keyof typeof languageTranslations];
   };
 
   return (
