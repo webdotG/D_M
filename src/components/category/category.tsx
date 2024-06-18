9a2dda1541516e059096f59a9eb9f16be54fd16e
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-19 00:20:00 +0300

    add store categorys sort home page

--- src/components/category/category.tsx
+++ src/components/category/category.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react';
-import { Categories } from '../../types';
+import { useCategoryStore } from '../../store';
 import style from './category.module.scss';
 
-const category: Categories = {
+const category = {
   dreams: {
-    1: 'Секс',
-    2: 'Плохое настроение',
-    3: 'Падения',
-    4: 'Бег',
+    1: 'D-1',
+    2: 'D-2',
+    3: 'D-3',
+    4: 'D-4',
   },
   memorises: {
-    1: 'детство',
-    2: 'отрочество',
-    3: 'юность',
-    4: 'взросление',
+    1: 'M-1',
+    2: 'M-2',
+    3: 'M-3',
+    4: 'M-4',
   }
 };
 
 export default function Category() {
-  const [currentCategory, setCurrentCategory] = useState<'dreams' | 'memorises'>('dreams');
+  const { selectedCategory, setSelectedCategory } = useCategoryStore();
 
   const toggleCategory = () => {
-    setCurrentCategory(currentCategory === 'dreams' ? 'memorises' : 'dreams');
+    setSelectedCategory(selectedCategory === 'сны' ? 'воспоминания' : 'сны');
   };
+
+  const currentCategory = selectedCategory === 'сны' ? 'dreams' : 'memorises';
 
   return (
     <section className={style['category']}>
       КАТЕГОРИИ
-      <button onClick={toggleCategory}>перключение тест</button>
+      <button onClick={toggleCategory}>переключение тест</button>
       {
         Object.entries(category[currentCategory]).map(([key, value]) => (
           <button className={style['category-button']} key={key}>{value}</button>
