c819bab17107b4322a48e2d56192c5bcf79f8aa6
Grant Kirill <webdotg.inbox@gmail.com>
2024-06-19 06:15:51 +0300

    icon footer add

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -42,9 +42,15 @@ type FooterProps = {
 
 const Footer: React.FC<FooterProps> = ({ translate }) => {
   return (
- <footer className={styles.footer}>
+    <footer className={styles.footer}>
       <button className={styles['footer-add']}>
-        <img src={HomeIcon} />
+        <svg width="52px" height="52px" viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <g id="layer1">
+            <path d="M 7 1 L 7 7 L 1 7 L 1 13 L 7 13 L 7 19 L 13 19 L 13 13 L 19 13 L 19 7 L 13 7 L 13 1 L 7 1 z M 8 2 L 12 2 L 12 8 L 18 8 L 18 12 L 12 12 L 12 18 L 8 18 L 8 12 L 2 12 L 2 8 L 8 8 L 8 2 z " 
+            fill='#222222'
+             />
+          </g>
+        </svg>
       </button>
       <ul className={styles['footer-list']}>
         {BUTTONS.map(item => (
