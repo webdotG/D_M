af30526e27c5fbb0e8ef4bc6b85d688a750abb2a
webdotg <webdotg@gmail.com>
2024-07-01 05:17:33 +0300

    fix conection sqlLite

--- D_M_LiteSQL/db/setup.ts
+++ D_M_LiteSQL/db/setup.ts
@@ -11,19 +11,30 @@ export async function openDb(dbPath: string): Promise<Database> {
 
 async function setup() {
   try {
-    const dbPath = path.resolve(__dirname, 'dreams_memories.db'); 
+    const dbPath = path.resolve(__dirname, './dreams_memories.db');  // Путь к базе данных
     const db = await openDb(dbPath);
     await db.exec(`
       CREATE TABLE IF NOT EXISTS dreams (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         category TEXT NOT NULL,
-        association TEXT NOT NULL,
+        associated TEXT NOT NULL,
         date TEXT NOT NULL,
         content TEXT NOT NULL,
-        isAnalyzed BOOLEAN NOT NULL
+        isAnalyzed BOOLEAN NOT NULL,
+        createdAt TEXT DEFAULT (datetime('now')),
+        updatedAt TEXT DEFAULT (datetime('now'))
       );
     `);
-    console.log('Database and tables created successfully');
+    console.log('setupTs db DMSqlLit : ', db.get.name)
+    await db.exec(`
+      CREATE TRIGGER IF NOT EXISTS update_dreams_updatedAt
+      AFTER UPDATE ON dreams
+      FOR EACH ROW
+      BEGIN
+        UPDATE dreams SET updatedAt = datetime('now') WHERE id = old.id;
+      END;
+    `);
+    console.log('Database and tables created successfully with triggers');
   } catch (error) {
     console.error('Error setting up the database:', error);
   }
