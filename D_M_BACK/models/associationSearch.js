ae517b240db3b0cd117271c276158b900f581816
webdotg <webdotg@gmail.com>
2024-07-01 06:00:58 +0300

    fix connect LiteSQL some ))) Test

--- D_M_BACK/models/associationSearch.js
+++ D_M_BACK/models/associationSearch.js
@@ -1,28 +1,24 @@
 import { dbLite } from '../dbLite.js';
-// console.log('{ dbLite } : ', dbLite );
-
 
 export const associationSearch = async (category) => {
-   
-  try {
-    const sql = `SELECT associations FROM dreams_memories WHERE associations != ''`;
+    try {
+        const sql = `SELECT associations FROM dreams_memories WHERE category = ? AND associations != ''`;
 
-    const params = [category];
-    const rows = await dbLite.all(sql, params);
-  
-    // Маппим значения associations, предполагая, что они хранятся в виде JSON-строк
-    const associations = rows.map(row => {
-      try {
-        return JSON.parse(row.associations);
-      } catch (error) {
-        console.error('Ошибка при парсинге JSON:', error);
-        return null; 
-      }
-    });
+        const rows = await dbLite.all(sql, [category]);
 
-    return associations;
-  } catch (error) {
-    console.error('Ошибка получения ассоциаций:', error);
-    throw error;
-  }
+        // Маппим значения associations, предполагая, что они хранятся в виде JSON-строк
+        const associations = rows.map(row => {
+            try {
+                return JSON.parse(row.associations);
+            } catch (error) {
+                console.error('Ошибка при парсинге JSON:', error);
+                return null;
+            }
+        });
+
+        return associations;
+    } catch (error) {
+        console.error('Ошибка получения ассоциаций:', error);
+        throw error;
+    }
 };
